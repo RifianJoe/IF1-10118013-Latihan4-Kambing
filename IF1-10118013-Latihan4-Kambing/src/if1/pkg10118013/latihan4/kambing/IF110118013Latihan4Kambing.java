cbb34c8d50afcb69a0b002866b723a0f35c09215
RifianJoe <55788355+RifianJoe@users.noreply.github.com>
2019-09-26 15:05:41 +0700

    Add files via upload

---
+++ IF1-10118013-Latihan4-Kambing/src/if1/pkg10118013/latihan4/kambing/IF110118013Latihan4Kambing.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan4.kambing;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan4Kambing {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+    
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah : " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IF110118013Latihan4Kambing kambingJantan = new IF110118013Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
